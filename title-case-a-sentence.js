c415c9cb14f112e7947cd12e32f5e89e81c02c81
Vicky <26657424+viizhu@users.noreply.github.com>
2017-11-14 16:18:49 -0500

    Added URL to freeCodeCamp exercise

--- title-case-a-sentence.js
+++ title-case-a-sentence.js
@@ -1,6 +1,7 @@
 /*
 * Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
 * For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
+* * * https://www.freecodecamp.org/challenges/title-case-a-sentence
 */
 
 function titleCase(str) {
