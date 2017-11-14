adaabc808d4aa14db8f86106f01956d658dc11f8
Vicky <26657424+viizhu@users.noreply.github.com>
2017-11-14 16:19:39 -0500

    syntax modified a bit

--- check-for-palindromes.js
+++ check-for-palindromes.js
@@ -4,7 +4,7 @@
 * Note: You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything lower case in order to check for palindromes.
 * We'll pass strings with varying formats, such as "racecar", "RaceCar", and "race CAR" among others.
 * We'll also pass strings with special symbols, such as "2A3*3a2", "2A3  3a2", and "2_A3*3#A2".
-* https://www.freecodecamp.org/challenges/check-for-palindromes
+* * * https://www.freecodecamp.org/challenges/check-for-palindromes
 */
 
 function palindrome(str) {  
